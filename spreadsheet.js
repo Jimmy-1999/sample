487684285c93261a55f2cb9af925c028652d5bae
Jimmy-1999 <109503051+Jimmy-1999@users.noreply.github.com>
2022-07-18 18:29:22 +0800

    Create spreadsheet.js

---
+++ spreadsheet.js
@@ -0,0 +1,5 @@
+const sheetId = '1dzx8QSiBQFcUOQTPa_uI64OCGQqESWtrq6EwqOVngZw';
+const base = `https://docs.google.com/spreadsheets/d/${18J56ryzqdUxr2IfZsjj3U8QI8dHxvQJMG9vXvFc1Aww}/gviz/tq?`;
+const sheetName = 'Sheet1';
+const query = encodeURIComponent('Select *')
+const url = `${base}&sheet=${sheetName}&tq=${query}`
